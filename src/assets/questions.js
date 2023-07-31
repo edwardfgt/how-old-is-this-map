7247186de83c8c8e07f9be4f54574eb8c5f5de78
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-07-31 18:05:12 +0100

    Create small sample of test questions

---
+++ src/assets/questions.js
@@ -0,0 +1,64 @@
+{
+  "questions": [
+    {
+      "id": 1,
+      "text": "Istanbul or Constantinople?",
+      "answers": [
+        {
+          "text": "Istanbul",
+          "followUpQuestionId": 47
+        },
+        {
+          "text": "Constantinople",
+          "followUpQuestionId": 10
+        },
+        {
+          "text": "Neither",
+          "followUpQuestionId": 2
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "text": "Does the Ottoman Empire exist?",
+      "answers": [
+        {
+          "text": "Yes",
+          "followUpQuestionId": 10
+        },
+        {
+          "text": "No",
+          "followUpQuestionId": 3
+        }
+      ]
+    },
+    {
+      "id": 10,
+      "text": "Do any of these exist? - Independent Canada, US territory of Alaska, Tokyo",
+      "answers": [
+        {
+          "text": "Yes 1900",
+          "followUpQuestionId": 23
+        },
+        {
+          "text": "No",
+          "followUpQuestionId": 11
+        }
+      ]
+    },
+    {
+      "id": 47,
+      "text": "Does the Soviet Union exist?",
+      "answers": [
+        {
+          "text": "Yes",
+          "followUpQuestionId": 5
+        },
+        {
+          "text": "No",
+          "followUpQuestionId": 48
+        }
+      ]
+    }
+  ]
+}
